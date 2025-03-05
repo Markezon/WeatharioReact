b5c5370e53e63e924ef0aa6486dac368a9c60a8a
Dmitry <neopatogen@gmail.com>
2025-03-05 03:08:15 +0100

    added errorMeassage/ airQuality

--- src/components/airQuality/AirQuality.js
+++ src/components/airQuality/AirQuality.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 import WeatherService from "../../services/WeatherService";
 
 class AirQuality extends Component {
@@ -10,7 +11,8 @@ class AirQuality extends Component {
 
   state = {
     data: {},
-    loading: false,
+    loading: true,
+    error: false,
     /*     co: null,
     no: null,
     no2: null,
@@ -24,81 +26,99 @@ class AirQuality extends Component {
   weatherService = new WeatherService();
 
   onDataLoaded = (data) => {
-    this.setState({ data });
+    this.setState({
+      data,
+      loading: false,
+    });
+  };
+
+  onError = () => {
+    this.setState({
+      loading: false,
+      error: true,
+    });
   };
 
   updateAirDetails = () => {
-    this.weatherService.getWeatherAirDetails().then(this.onDataLoaded);
+    this.weatherService
+      .getWeatherAirDetails()
+      .then(this.onDataLoaded)
+      .catch(this.onError);
   };
 
   render() {
-    const {
-      data: { co, no, no2, o3, so2, pm2_5, pm10, nh3, aqi },
-      loading,
-    } = this.state;
-    const aqiList = ["Good", "Fair", "Moderate", "Poor", "Very Poor"];
-    let aqiClass = `air-index aqi-${aqi}`;
+    const { data, loading, error } = this.state;
 
-    if (loading) {
-      return (
-        <div className="card">
-          <Spinner />
-        </div>
-      );
-    }
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? <View data={data} /> : null;
 
     return (
       <div className="card">
-        <div className="card-head">
-          <p>Air Quality Index</p>
-          <p className={aqiClass}>{aqiList[aqi - 1]}</p>
-        </div>
-
-        <div className="air-indices">
-          <i className="fa-regular fa-wind fa-3x"></i>
-          <div className="item">
-            <p>PM2.5</p>
-            <h2>{pm2_5}</h2>
-          </div>
-
-          <div className="item">
-            <p>PM10</p>
-            <h2>{pm10}</h2>
-          </div>
-
-          <div className="item">
-            <p>SO2</p>
-            <h2>{so2}</h2>
-          </div>
-
-          <div className="item">
-            <p>CO</p>
-            <h2>{co}</h2>
-          </div>
-
-          <div className="item">
-            <p>NO</p>
-            <h2>{no}</h2>
-          </div>
-
-          <div className="item">
-            <p>NO2</p>
-            <h2>{no2}</h2>
-          </div>
-
-          <div className="item">
-            <p>NH3</p>
-            <h2>{nh3}</h2>
-          </div>
-
-          <div className="item">
-            <p>O3</p>
-            <h2>{o3}</h2>
-          </div>
-        </div>
+        {errorMessage}
+        {spinner}
+        {content}
       </div>
     );
   }
 }
+
+const View = ({ data }) => {
+  const { co, no, no2, o3, so2, pm2_5, pm10, nh3, aqi } = data;
+  const aqiList = ["Good", "Fair", "Moderate", "Poor", "Very Poor"];
+  let aqiClass = `air-index aqi-${aqi}`;
+
+  return (
+    <>
+      <div className="card-head">
+        <p>Air Quality Index</p>
+        <p className={aqiClass}>{aqiList[aqi - 1]}</p>
+      </div>
+
+      <div className="air-indices">
+        <i className="fa-regular fa-wind fa-3x"></i>
+        <div className="item">
+          <p>PM2.5</p>
+          <h2>{pm2_5}</h2>
+        </div>
+
+        <div className="item">
+          <p>PM10</p>
+          <h2>{pm10}</h2>
+        </div>
+
+        <div className="item">
+          <p>SO2</p>
+          <h2>{so2}</h2>
+        </div>
+
+        <div className="item">
+          <p>CO</p>
+          <h2>{co}</h2>
+        </div>
+
+        <div className="item">
+          <p>NO</p>
+          <h2>{no}</h2>
+        </div>
+
+        <div className="item">
+          <p>NO2</p>
+          <h2>{no2}</h2>
+        </div>
+
+        <div className="item">
+          <p>NH3</p>
+          <h2>{nh3}</h2>
+        </div>
+
+        <div className="item">
+          <p>O3</p>
+          <h2>{o3}</h2>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default AirQuality;
